2ae2726084310ac27978918aacb041d68be5185f
handeyed <jonathan.jato@gmail.com>
2016-11-20 21:01:32 +0100

    Test Webhook

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,23 @@
-$(function() {                       
-  $(".get-sarted-steps").click(function() {  
-    $(this).addClass("active").removeClass("undisplayed");
+$(document).ready(function(){
+
+  $('.testimonial-slide').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    speed: 800,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    mobileFirst: true
+  });
+
+  $(".menu-opener").click(function(){
+    $(".menu-opener-inner").toggleClass("active")
+    $("#content").toggleClass("active");
+  });
+  $("#steps div").click(function(){
+    $( "#steps div" ).removeClass('active');
+    $(this).addClass(" active");
   });
 });
